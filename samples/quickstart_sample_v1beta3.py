b01d8027c2d99619b4282a6cf62265ac0133a40f
Ariana Bray <aribray@google.com>
2020-10-15 08:09:16 -0700

    add quick start and process_document samples and tests

---
+++ samples/quickstart_sample_v1beta3.py
@@ -0,0 +1,78 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from google.cloud import documentai_v1beta3 as documentai
+
+project_id = 'python-docs-samples-tests'
+location = 'us'
+processor_id = '90484cfdedb024f6'
+file_path = '/Users/erschmid/SampleMedia/invoice.pdf'
+
+# [START documentai_quickstart_sample]
+
+# TODO(developer): Uncomment these variables before running the sample.
+# project_id= 'YOUR_PROJECT_ID';
+# location = 'YOUR_PROJECT_LOCATION'; // Format is 'us' or 'eu'
+# processor_id = 'YOUR_PROCESSOR_ID'; // Create processor in Cloud Console
+# file_path = '/path/to/local/pdf';
+
+
+def quickstart(project_id: str, location: str, processor_id: str, file_path: str):
+    client_options = {
+        'api_endpoint': 'us-documentai.googleapis.com'
+    }
+    client = documentai.DocumentProcessorServiceClient(client_options=client_options)
+
+    # The full resource name of the processor, e.g.:
+    # projects/project-id/locations/location/processor/processor-id
+    # You must create new processors in the Cloud Console first
+    name = f'projects/{project_id}/locations/{location}/processors/{processor_id}'
+
+    # Read the file into memory
+    with open(file_path, 'rb') as image:
+        image_content = image.read()
+
+    document = {
+        'content': image_content,
+        'mime_type': 'application/pdf'
+    }
+
+    # Configure the process request
+    request = {
+        'name': name,
+        'document': document
+    }
+
+    result = client.process_document(request=request)
+
+    document = result.document
+    text = document.text
+
+    page_1 = document.pages[0]
+    paragraphs = page_1.paragraphs
+
+    for paragraph in paragraphs:
+        paragraph_text = get_text(paragraph.layout.text_anchor, text)
+        print(f'Paragraph text: {paragraph_text}')
+
+
+def get_text(text_anchor: dict, text: str):
+    # First shard in document doesn't have startIndex property
+    start_index = text_anchor.text_segments[0].start_index if text_anchor.text_segments[0].start_index is not None else 0
+    end_index = text_anchor.text_segments[0].end_index
+
+    return text[start_index:end_index]
+
+# [END documentai_quickstart_sample]
---
+++ tests/unit/gapic/documentai_v1beta3/process_document_v1beta3_test.py
@@ -0,0 +1,73 @@
+# # Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from uuid import uuid4
+import pytest
+import sys, os
+from google.cloud import storage
+import google.api_core as api_core
+
+from samples import process_document_sample_v1beta3
+
+from google.cloud.documentai_v1beta3.services.document_processor_service import (
+    DocumentProcessorServiceClient,
+)
+from google.cloud.documentai_v1beta3.services.document_processor_service import (
+    transports,
+)
+from google.cloud.documentai_v1beta3.types import document
+from google.cloud.documentai_v1beta3.types import document_processor_service
+from google.cloud.documentai_v1beta3.types import geometry
+from google.cloud.documentai_v1beta3.types import ProcessRequest
+
+
+location = "us"
+project_id = '1012616486416'
+processor_id = '90484cfdedb024f6'
+bucket_name = 'python_docs_samples_test_%s' %uuid4()
+gcs_input_uri ='gs://cloud-samples-data/documentai/invoice.pdf'
+gcs_output_uri = 'output-bucket'
+gcs_output_uri_prefix = uuid4()
+file_name = 'samples/resources/invoice.pdf'
+file_path = os.path.join(os.getcwd(), file_name)
+
+name = "projects/1012616486416/locations/us/processors/90484cfdedb024f6"
+
+client_options = dict(api_endpoint="us-documentai.googleapis.com")
+client = DocumentProcessorServiceClient(client_options=client_options)
+
+@pytest.fixture(scope="module")
+def setup():
+	storage_client = storage.Client()
+	storage_client.bucket(bucket_name)
+
+@pytest.fixture(scope="module")
+def tear_down():
+	storage_client = storage.Client()
+	bucket = storage_client.bucket(bucket_name)
+	blobs = bucket.list_blobs(prefix=gcs_output_uri_prefix)
+
+	for blob in blobs:
+		blob.delete()
+
+	bucket.delete()
+
+def test_process_documents(capsys):
+	result = process_document_sample_v1beta3.process_document_sample(project_id=project_id, location=location, processor_id=processor_id, file_path=file_path)
+	out, err = capsys.readouterr()
+	sys.stdout.write(out)
+	sys.stderr.write(err)
+
+	assert "Paragraph" in out
