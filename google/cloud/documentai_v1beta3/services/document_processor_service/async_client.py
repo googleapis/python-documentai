3590a807873bd272f9a9ce517319536fdcdef8fb
yoshi-automation <yoshi-automation@google.com>
2021-03-13 03:04:58 -0800

    chore: update gapic-generator-python to 0.40.11
    PiperOrigin-RevId: 359562873
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Feb 25 10:52:32 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 07932bb995e7dc91b43620ea8402c6668c7d102c
    Source-Link: https://github.com/googleapis/googleapis/commit/07932bb995e7dc91b43620ea8402c6668c7d102c

--- google/cloud/documentai_v1beta3/services/document_processor_service/async_client.py
+++ google/cloud/documentai_v1beta3/services/document_processor_service/async_client.py
@@ -95,8 +95,36 @@ class DocumentProcessorServiceAsyncClient:
         DocumentProcessorServiceClient.parse_common_location_path
     )
 
-    from_service_account_info = DocumentProcessorServiceClient.from_service_account_info
-    from_service_account_file = DocumentProcessorServiceClient.from_service_account_file
+    @classmethod
+    def from_service_account_info(cls, info: dict, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials info.
+
+        Args:
+            info (dict): The service account private key info.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            DocumentProcessorServiceAsyncClient: The constructed client.
+        """
+        return DocumentProcessorServiceClient.from_service_account_info.__func__(DocumentProcessorServiceAsyncClient, info, *args, **kwargs)  # type: ignore
+
+    @classmethod
+    def from_service_account_file(cls, filename: str, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials
+        file.
+
+        Args:
+            filename (str): The path to the service account private key json
+                file.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            DocumentProcessorServiceAsyncClient: The constructed client.
+        """
+        return DocumentProcessorServiceClient.from_service_account_file.__func__(DocumentProcessorServiceAsyncClient, filename, *args, **kwargs)  # type: ignore
+
     from_service_account_json = from_service_account_file
 
     @property
